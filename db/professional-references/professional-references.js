94b891d0e28cb579042daab9e233f6fd5d70d613
“graciegould” <graciegould5@gmail.com>
2024-10-11 12:53:55 -0700

    remove file

--- db/professional-references/professional-references.js
+++
@@ -1,8 +0,0 @@
-import Model from  '../model.js';
-class ProfessionalReferences extends Model {
-    constructor() {
-        super('professionalReferences');
-    }
-}
-
-export default ProfessionalReferences;
